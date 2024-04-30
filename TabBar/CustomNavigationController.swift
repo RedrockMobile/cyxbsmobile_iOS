ef74b5ef6c3452fdd12ecb17c22b6d527f9fed5d
Max Xu <114380848+Max-Xu2004@users.noreply.github.com>
2024-04-30 19:01:00 +0800

    [optimize]pop转场使用默认动画和手势
    自定义转场太难了，毁灭吧

--- TabBar/CustomNavigationController.swift
+++ TabBar/CustomNavigationController.swift
@@ -9,68 +9,26 @@
 import UIKit
 
 class CustomNavigationController: UINavigationController, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
-
-    var popDelegate: UIGestureRecognizerDelegate?
-    var edgePan: UIScreenEdgePanGestureRecognizer?
-    var interactiveTransition:UIPercentDrivenInteractiveTransition?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.isNavigationBarHidden = true
-        self.interactivePopGestureRecognizer?.isEnabled = false
-        self.delegate = self
-        edgePan = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handlePan(_: )))
-        edgePan?.edges = .left
-        if let pan = edgePan {
-            view.addGestureRecognizer(pan)
-        }
+        isNavigationBarHidden = true
+        interactivePopGestureRecognizer?.isEnabled = true
+        interactivePopGestureRecognizer?.delegate = self
+        delegate = self
     }
     
-    @objc func handlePan(_ pan: UIPanGestureRecognizer) {
-        switch pan.state {
-        case .began:
-            // 检查是否已经回到根视图控制器
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer == interactivePopGestureRecognizer {
             if topViewController == viewControllers.first {
                 // 已经是根视图控制器，不执行返回操作，直接结束交互式转场
-                interactiveTransition?.cancel()
-                interactiveTransition = nil
+                return false
             } else {
                 // 开始新的交互式转场
-                interactiveTransition = UIPercentDrivenInteractiveTransition()
-                popViewController(animated: true)
+                return true
             }
-        case .changed:
-            if let interactiveTransition = interactiveTransition {
-                interactiveTransition.update(pan.translation(in: view).x / view.width)
-            }
-        case .ended, .cancelled:
-            if let interactiveTransition = interactiveTransition {
-                if pan.translation(in: view).x / view.width > 0.4 || pan.velocity(in: view).x > 300 {
-                    interactiveTransition.finish()
-                } else {
-                    interactiveTransition.cancel()
-                }
-                self.interactiveTransition = nil
-            }
-        
-        case .possible:
-            break
-        case .failed:
-            break
-        @unknown default:
-            break
         }
+        return true
     }
-
-    
-    // MARK: - UINavigationControllerDelegate
-    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> (any UIViewControllerAnimatedTransitioning)? {
-        return operation == .pop ? PopAnimation() : nil
-    }
-    
-    func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: any UIViewControllerAnimatedTransitioning) -> (any UIViewControllerInteractiveTransitioning)? {
-        return animationController is PopAnimation ? interactiveTransition : nil
-    }
-    
 }
 
