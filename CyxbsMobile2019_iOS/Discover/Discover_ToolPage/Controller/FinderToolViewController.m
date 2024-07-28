e607273dde46519a81803482a542fec728ddf1fd
Max Xu <114380848+Max-Xu2004@users.noreply.github.com>
2024-07-28 21:10:43 +0800

    [feature]恢复体育打卡和考试成绩代码

--- CyxbsMobile2019_iOS/Discover/Discover_ToolPage/Controller/FinderToolViewController.m
+++ CyxbsMobile2019_iOS/Discover/Discover_ToolPage/Controller/FinderToolViewController.m
@@ -14,6 +14,8 @@
 //#import "WeDateViewController.h"
 #import "CQUPTMapViewController.h"
 #import "TODOMainViewController.h"
+#import "TestArrangeViewController.h"
+#import "SportAttendanceViewController.h"
 #import "掌上重邮-Swift.h"
 
 @interface FinderToolViewController () <
@@ -217,7 +219,9 @@
     FinderToolViewItem *item4 = [[FinderToolViewItem alloc]initWithIconView:@"校历" Title:@"校历" Detail:@"学期安排一目了然"];
     FinderToolViewItem *item5 = [[FinderToolViewItem alloc]initWithIconView:@"重邮地图" Title:@"重邮地图" Detail:@"校园地图，尽收重邮风光"];
     FinderToolViewItem *item6 = [[FinderToolViewItem alloc] initWithIconView:@"邮子清单" Title:@"邮子清单" Detail:@"邮子清单"];
-    FinderToolViewItem *item7 = [[FinderToolViewItem alloc]initWithIconView:@"更多功能" Title:@"更多功能" Detail:@"敬请期待"];
+    FinderToolViewItem *item7 = [[FinderToolViewItem alloc]initWithIconView:@"我的考试" Title:@"我的考试" Detail:@"考试安排、成绩学分轻松查询"];
+    FinderToolViewItem *item8 = [[FinderToolViewItem alloc] initWithIconView:@"体育打卡" Title:@"体育打卡" Detail:@"体育打卡"];
+    FinderToolViewItem *item9 = [[FinderToolViewItem alloc]initWithIconView:@"更多功能" Title:@"更多功能" Detail:@"敬请期待"];
     
     [item1 addTarget:self action:@selector(chooseWeDate:) forControlEvents:UIControlEventTouchUpInside];
     [item2 addTarget:self action:@selector(chooseSchoolBus:) forControlEvents:UIControlEventTouchUpInside];
@@ -225,6 +229,8 @@
     [item4 addTarget:self action:@selector(chooseSchoolSchedule:) forControlEvents:UIControlEventTouchUpInside];
     [item5 addTarget:self action:@selector(chooseCQUPTMap:) forControlEvents:UIControlEventTouchUpInside];
     [item6 addTarget:self action:@selector(chooseToDo:) forControlEvents:UIControlEventTouchUpInside];
+    [item7 addTarget:self action:@selector(chooseTestArrange:) forControlEvents:UIControlEventTouchUpInside];
+    [item8 addTarget:self action:@selector(chooseSportAttendance:) forControlEvents:UIControlEventTouchUpInside];
     
     NSMutableArray *itemsArray = [NSMutableArray array];
     [itemsArray addObject:item1];
@@ -234,6 +240,8 @@
     [itemsArray addObject:item5];
     [itemsArray addObject:item6];
     [itemsArray addObject:item7];
+    [itemsArray addObject:item8];
+    [itemsArray addObject:item9];
     
     self.toolViewItems = itemsArray;
     for (FinderToolViewItem*item in self.toolViewItems) {
@@ -282,15 +290,8 @@
     if (sender.isChooingNow == YES) {
         [sender toggleFavoriteStates];
     } else {
-//        UserItem *item = [UserItem defaultItem];
-//        //点击了没课约
-//        NSDictionary *dic = @{
-//            @"name" : item.realName,
-//            @"stuNum" : item.stuNum
-//        };
-//        WeDateViewController *vc = [[WeDateViewController alloc] initWithInfoDictArray:@[dic].modelCopy];
+        //点击了没课约
         WeDateVC *vc = [[WeDateVC alloc] init];
-        
         [self.navigationController pushViewController:vc animated:YES];
     }
 }
@@ -346,6 +347,28 @@
         [sender toggleFavoriteStates];
     } else {
         TODOMainViewController *vc = [[TODOMainViewController alloc] init];
+        vc.hidesBottomBarWhenPushed = YES;
+        [self.navigationController pushViewController:vc animated:YES];
+    }
+}
+
+/// 我的考试
+- (void)chooseTestArrange:(FinderToolViewItem *)sender  {
+    if (sender.isChooingNow == YES) {
+        [sender toggleFavoriteStates];
+    }else {
+        TestArrangeViewController *vc = [[TestArrangeViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+        
+    }
+}
+
+/// 体育打卡
+- (void)chooseSportAttendance:(FinderToolViewItem *)sender{
+    if (sender.isChooingNow == YES) {
+        [sender toggleFavoriteStates];
+    } else {
+        SportAttendanceViewController *vc = [[SportAttendanceViewController alloc] init];
         vc.hidesBottomBarWhenPushed = YES;
         [self.navigationController pushViewController:vc animated:YES];
     }
